4e81d4609dba59ca54572db51174d2222042e20a
Jacob O'Bryant <jacobobryant7@gmail.com>
2017-06-08 16:42:46 -0600

    bar

--- pos.py
+++ pos.py
@@ -5,19 +5,19 @@ from fractions import Fraction as frac
 def viterbi(obs, states, start_p, trans_p, emit_p):
     V = [{}]
     for st in states:
-        V[0][st] = {"prob": start_p[st] * emit_p[st].get(obs[0], 0.001), "prev": None}
+        V[0][st] = {"prob": start_p[st] * emit_p[st].get(obs[0], 0), "prev": None}
     # Run Viterbi when t > 0
     for t in range(1, len(obs)):
         V.append({})
         for st in states:
-            max_tr_prob = max(V[t-1][prev_st]["prob"]*trans_p[prev_st].get(st, 0.001) for prev_st in states)
+            max_tr_prob = max(V[t-1][prev_st]["prob"]*trans_p[prev_st].get(st, 0) for prev_st in states)
             for prev_st in states:
-                if V[t-1][prev_st]["prob"] * trans_p[prev_st].get(st, 0.001) == max_tr_prob:
-                    max_prob = max_tr_prob * emit_p[st].get(obs[t], 0.001)
+                if V[t-1][prev_st]["prob"] * trans_p[prev_st].get(st, 0) == max_tr_prob:
+                    max_prob = max_tr_prob * emit_p[st].get(obs[t], 0)
                     V[t][st] = {"prob": max_prob, "prev": prev_st}
                     break
-    for line in dptable(V):
-        print(line)
+    #for line in dptable(V):
+    #    print(line)
     opt = []
     # The highest probability
     max_prob = max(value["prob"] for value in V[-1].values())
@@ -33,7 +33,8 @@ def viterbi(obs, states, start_p, trans_p, emit_p):
         opt.insert(0, V[t + 1][previous]["prev"])
         previous = V[t + 1][previous]["prev"]
 
-    print('The steps of states are ' + ' '.join(opt) + ' with highest probability of %s' % max_prob)
+    return opt
+    #print('The steps of states are ' + ' '.join(opt) + ' with highest probability of %s' % max_prob)
 
 def dptable(V):
     # Print a table of steps from dictionary
@@ -41,32 +42,33 @@ def dptable(V):
     for state in V[0]:
         yield "%.7s: " % state + " ".join("%.7s" % ("%f" % v[state]["prob"]) for v in V)
 
-def main(training_file):
+def read_words(filename):
     Word = namedtuple('Word', 'word pos')
 
-    with open(training_file, 'r') as f:
-        sentence_list = [[Word(*word.rsplit('_', 1)) for word in sentence.split()]
-                         for sentence in f.read().splitlines()]
+    with open(filename, 'r') as f:
+        return [Word(*word.rsplit('_', 1)) for word in f.read().split()]
+
+def main(training_file, testing_file):
+    word_list = read_words(training_file)
 
     transitions = {}
     emissions = {}
 
-    for sentence in sentence_list:
-        # count the transitions
-        for previous, current in zip(sentence[:-1], sentence[1:]):
-            if previous.pos not in transitions:
-                transitions[previous.pos] = {}
-            if current.pos not in transitions[previous.pos]:
-                transitions[previous.pos][current.pos] = 0
-            transitions[previous.pos][current.pos] += 1
+    # count the transitions
+    for previous, current in zip(word_list[:-1], word_list[1:]):
+        if previous.pos not in transitions:
+            transitions[previous.pos] = {}
+        if current.pos not in transitions[previous.pos]:
+            transitions[previous.pos][current.pos] = 0
+        transitions[previous.pos][current.pos] += 1
 
-        # count the emissions
-        for word in sentence:
-            if word.pos not in emissions:
-                emissions[word.pos] = {}
-            if word.word not in emissions[word.pos]:
-                emissions[word.pos][word.word] = 0
-            emissions[word.pos][word.word] += 1
+    # count the emissions
+    for word in word_list:
+        if word.pos not in emissions:
+            emissions[word.pos] = {}
+        if word.word not in emissions[word.pos]:
+            emissions[word.pos][word.word] = 0
+        emissions[word.pos][word.word] += 1
 
     # normalize the counts
     for structure in [transitions, emissions]:
@@ -77,15 +79,24 @@ def main(training_file):
     
     states = set(emissions.keys())
     start_p = {state: frac(1, len(states)) for state in states}
+    
+    hits = 0
+    misses = 0
+    testing_words = read_words(testing_file)
 
-    viterbi("The quick brown fox jumps over the lazy dog .".split(),
-            states,
-            start_p,
-            transitions,
-            emissions)
+    observations = [word.word for word in testing_words]
+    predictions = viterbi(observations, states, start_p, transitions, emissions)
+    for observation, prediction in zip(testing_words, predictions):
+        if observation.pos == prediction:
+            hits += 1
+        else:
+            misses += 1
+
+    print(hits / (hits + misses))
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--training", '-f', default="training_dataset.txt")
+    parser.add_argument("--testing", '-t', default="testing_dataset.txt")
     args = parser.parse_args()
-    main(args.training)
+    main(args.training, args.testing)
