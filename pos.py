d6a22bb7396774762e2b5ee392e0620298139dfb
Jacob O'Bryant <jacobobryant7@gmail.com>
2017-06-08 22:58:32 -0600

    print best, worst sentences

--- pos.py
+++ pos.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from collections import namedtuple
 from fractions import Fraction as frac
+from heapq import nlargest, nsmallest
 
 def viterbi(obs, states, start_p, trans_p, emit_p):
     V = [{}]
@@ -42,33 +43,42 @@ def dptable(V):
     for state in V[0]:
         yield "%.7s: " % state + " ".join("%.7s" % ("%f" % v[state]["prob"]) for v in V)
 
-def read_words(filename):
+def read_sentences(filename):
     Word = namedtuple('Word', 'word pos')
 
     with open(filename, 'r') as f:
-        return [Word(*word.rsplit('_', 1)) for word in f.read().split()]
+        return [[Word(*word.rsplit('_', 1)) for word in sentence.split()]
+                for sentence in f.read().splitlines()]
+
+def print_result(r):
+    print(' '.join(word.word for word in r.test_data))
+    print("    actual: " + ' '.join(word.pos for word in r.test_data))
+    print("prediction: " + ' '.join(pos for pos in r.predictions))
+    print("     score: {}".format(r.score))
+    print()
 
 def main(training_file, testing_file):
-    word_list = read_words(training_file)
+    sentence_list = read_sentences(training_file)
 
     transitions = {}
     emissions = {}
 
     # count the transitions
-    for previous, current in zip(word_list[:-1], word_list[1:]):
-        if previous.pos not in transitions:
-            transitions[previous.pos] = {}
-        if current.pos not in transitions[previous.pos]:
-            transitions[previous.pos][current.pos] = 0
-        transitions[previous.pos][current.pos] += 1
+    for sentence in sentence_list:
+        for previous, current in zip(sentence[:-1], sentence[1:]):
+            if previous.pos not in transitions:
+                transitions[previous.pos] = {}
+            if current.pos not in transitions[previous.pos]:
+                transitions[previous.pos][current.pos] = 0
+            transitions[previous.pos][current.pos] += 1
 
-    # count the emissions
-    for word in word_list:
-        if word.pos not in emissions:
-            emissions[word.pos] = {}
-        if word.word not in emissions[word.pos]:
-            emissions[word.pos][word.word] = 0
-        emissions[word.pos][word.word] += 1
+        # count the emissions
+        for word in sentence:
+            if word.pos not in emissions:
+                emissions[word.pos] = {}
+            if word.word not in emissions[word.pos]:
+                emissions[word.pos][word.word] = 0
+            emissions[word.pos][word.word] += 1
 
     # normalize the counts
     for structure in [transitions, emissions]:
@@ -80,19 +90,42 @@ def main(training_file, testing_file):
     states = set(emissions.keys())
     start_p = {state: frac(1, len(states)) for state in states}
     
-    hits = 0
-    misses = 0
-    testing_words = read_words(testing_file)
+    total_hits = 0
+    total_misses = 0
+    test_sentences = read_sentences(testing_file)
+    results = []
+    Result = namedtuple("Result", "test_data predictions score")
 
-    observations = [word.word for word in testing_words]
-    predictions = viterbi(observations, states, start_p, transitions, emissions)
-    for observation, prediction in zip(testing_words, predictions):
-        if observation.pos == prediction:
-            hits += 1
-        else:
-            misses += 1
+    for i, sentence in enumerate(test_sentences):
+        print("\r{}%".format((i*100) // len(test_sentences)), end="")
 
-    print(hits / (hits + misses))
+        hits = 0
+        misses = 0
+        observations = [word.word for word in sentence]
+        predictions = viterbi(observations, states, start_p, transitions, emissions)
+
+        for observation, prediction in zip(sentence, predictions):
+            if observation.pos == prediction:
+                hits += 1
+            else:
+                misses += 1
+
+        score = hits / (hits + misses)
+        results.append(Result(sentence, predictions, score))
+        total_hits += hits
+        total_misses += misses
+
+    print("\n\nBEST:")
+    best = nlargest(10, results, key=lambda r: r.score)
+    for r in best:
+        print_result(r)
+
+    print("\nWORST:")
+    worst = nsmallest(10, results, key=lambda r: r.score)
+    for r in worst:
+        print_result(r)
+
+    print("total accuracy:", total_hits / (total_hits + total_misses))
 
 if __name__ == "__main__":
     parser = ArgumentParser()
